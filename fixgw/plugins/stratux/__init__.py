11cb0ebd97b7a2ae669242c0d8dfc2e79b7bef6d
Mike Sutton <mikesutton@gmail.com>
2020-06-24 17:27:55 -0600

    add detailed logging

--- fixgw/plugins/stratux/__init__.py
+++ fixgw/plugins/stratux/__init__.py
@@ -9,6 +9,8 @@ import struct
 import math
 
 
+DETAILED_DEBUG = True
+
 class MainThread(threading.Thread):
     def __init__(self, parent):
         super(MainThread, self).__init__()
@@ -19,9 +21,11 @@ class MainThread(threading.Thread):
 
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.s.bind(('', 4000))
+        self.log.debug("Listening socket on port 4000 from stratux created.")
 
     def run(self):
 
+        self.log.debug("Starting straux listening loop.")
         while not self.getout:
             msg, adr = self.s.recvfrom(8192)
             msg = gdl90.decodeGDL90(msg)
@@ -47,12 +51,16 @@ class MainThread(threading.Thread):
                 self.parent.db_write("ALAT", -math.sin(slipskid*math.pi/180))
                 self.parent.db_write("ALT", alt)
                 self.parent.db_write("VS", vs)
+                if DETAILED_DEBUG:
+                    self.log.debug(f"Stratux AHRS message: Roll: {roll}, pitch: {pitch}, heading: {heading}, slipskid: {slipskid}, yawrate: {yawrate}, g: {g}, alt: {alt}, vs: {vs}")
             elif msg[0] == 0x0a:
                 # ownship report
                 alt = struct.unpack('>h', msg[11:13])[0]
                 tmp = struct.unpack('BB', msg[14:16])
                 gnd_speed = (tmp[0] << 4) | (tmp[1] >> 4)
                 self.parent.db_write("IAS", gnd_speed)
+                if DETAILED_DEBUG:
+                    self.log.debug(f"Ownship: gnd speed: {gnd_speed}")
 
         self.running = False
 
